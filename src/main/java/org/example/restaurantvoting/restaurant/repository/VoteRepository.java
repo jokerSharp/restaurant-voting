111bbad2f866461b45b904f771bebfb882f3124d
jokerSharp <qasoftware@tutanota.com>
2024-12-28 21:43:36 +0300

    change vote so it has usual id instead of embedded and remove isDeleted field - no need for soft delete

--- src/main/java/org/example/restaurantvoting/restaurant/repository/VoteRepository.java
+++ src/main/java/org/example/restaurantvoting/restaurant/repository/VoteRepository.java
@@ -1,9 +1,8 @@
 package org.example.restaurantvoting.restaurant.repository;
 
+import org.example.restaurantvoting.common.BaseRepository;
 import org.example.restaurantvoting.restaurant.model.Vote;
-import org.example.restaurantvoting.restaurant.model.VoteId;
 import org.example.restaurantvoting.user.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,12 +12,12 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 @Transactional(readOnly = true)
-public interface VoteRepository extends JpaRepository<Vote, VoteId> {
+public interface VoteRepository extends BaseRepository<Vote> {
 
     Optional<Vote> findByUserAndCreatedAt(User user, LocalDate createdAt);
 
     @Transactional
     @Modifying
     @Query("update Vote v set v.restaurant.id=:restaurantId where v.user.id=:userId")
-    void updateVote(@Param("restaurantId") int restaurantId, @Param("userId") int userId);
+    void reVote(@Param("restaurantId") int restaurantId, @Param("userId") int userId);
 }
--- src/main/java/org/example/restaurantvoting/restaurant/service/VoteService.java
+++ src/main/java/org/example/restaurantvoting/restaurant/service/VoteService.java
@@ -1,9 +1,9 @@
 package org.example.restaurantvoting.restaurant.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.restaurantvoting.restaurant.model.Restaurant;
 import org.example.restaurantvoting.restaurant.model.Vote;
-import org.example.restaurantvoting.restaurant.model.VoteId;
 import org.example.restaurantvoting.restaurant.repository.VoteRepository;
 import org.example.restaurantvoting.user.model.User;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,12 +11,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 @AllArgsConstructor
+@Slf4j
 public class VoteService {
 
     private final VoteRepository repository;
@@ -24,11 +24,8 @@ public class VoteService {
     @Scheduled(cron = "0 0 0 * * ?")
     @Transactional
     public void inactivateVote() {
-        List<Vote> votes = repository.findAll();
-        for (Vote vote : votes) {
-            vote.setDeleted(true);
-            repository.save(vote);
-        }
+        log.info("delete votes by schedule");
+        repository.deleteAll();
     }
 
     public Optional<Vote> findCurrentDayVote(User user) {
@@ -37,13 +34,12 @@ public class VoteService {
 
     @Transactional
     public Vote save(Restaurant restaurant, User user) {
-        VoteId voteId = new VoteId(restaurant.getId(), user.getId());
-        Vote newVote = new Vote(voteId, restaurant, user);
+        Vote newVote = new Vote(restaurant, user);
         return repository.save(newVote);
     }
 
     @Transactional
     public void reVote(int restaurantId, int userId) {
-        repository.updateVote(restaurantId, userId);
+        repository.reVote(restaurantId, userId);
     }
 }
