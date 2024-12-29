b183885eb19a8c4fb0583edc1c52aa26e379f95f
jokerSharp <qasoftware@tutanota.com>
2024-12-29 20:39:17 +0300

    add DishControllerTest
    fix DishController issue with NotFound with get method

--- src/main/java/org/example/restaurantvoting/restaurant/repository/DishRepository.java
+++ src/main/java/org/example/restaurantvoting/restaurant/repository/DishRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Transactional(readOnly = true)
 public interface DishRepository extends BaseRepository<Dish> {
@@ -14,5 +15,5 @@ public interface DishRepository extends BaseRepository<Dish> {
     List<Dish> findAllByRestaurantId(int restaurantId);
 
     @Query("select d from Dish d where d.restaurant.id=:restaurantId and d.id=:dishId")
-    Dish findOneByRestaurantId(@Param("restaurantId") int restaurantId, @Param("dishId")int dishId);
+    Optional<Dish> findOneByRestaurantId(@Param("restaurantId") int restaurantId, @Param("dishId")int dishId);
 }
