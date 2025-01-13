8a38c5d83d8d6793c3495752f6a7c4e8c36ee75c
jokerSharp <qasoftware@tutanota.com>
2025-01-13 22:33:45 +0300

    add unique name validator for restaurant entity and tests for it

--- src/main/java/org/example/restaurantvoting/restaurant/repository/RestaurantRepository.java
+++ src/main/java/org/example/restaurantvoting/restaurant/repository/RestaurantRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Transactional(readOnly = true)
 public interface RestaurantRepository extends BaseRepository<Restaurant> {
 
     @Query("select r from Restaurant r left join fetch r.dishes d where d.actualityDate=current_date")
     List<Restaurant> findWithDishes();
+
+    Optional<Restaurant> findByName(String name);
 }
