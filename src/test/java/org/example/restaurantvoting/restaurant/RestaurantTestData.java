96e88175958a216e2e0b615c91f27a58d450bc3b
jokerSharp <qasoftware@tutanota.com>
2025-01-13 22:33:45 +0300

    add unique name validator for restaurant entity and tests for it

--- src/test/java/org/example/restaurantvoting/restaurant/RestaurantTestData.java
+++ src/test/java/org/example/restaurantvoting/restaurant/RestaurantTestData.java
@@ -8,10 +8,11 @@ public class RestaurantTestData {
     public static final MatcherFactory.Matcher<Restaurant> RESTAURANT_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(Restaurant.class, "dishes", "votes");
 
     public static final int RESTAURANT_1_ID = 1;
+    public static final String RESTAURANT_1_NAME = "Mirazur";
     public static final int RESTAURANT_2_ID = 2;
     public static final int NOT_FOUND = 100;
 
-    public static final Restaurant mirazur = new Restaurant (RESTAURANT_1_ID, "Mirazur", null, null);
+    public static final Restaurant mirazur = new Restaurant (RESTAURANT_1_ID, RESTAURANT_1_NAME, null, null);
     public static final Restaurant geranium = new Restaurant (RESTAURANT_2_ID, "Geranium", null, null);
 
     public static Restaurant getNew() {
